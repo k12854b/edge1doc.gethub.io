3baad12567cc9ed2ef7b7bbea0a7c8db388f86f1
k12854b <oc.yrn07@gmail.com>
2024-06-21 23:28:45 +0200

    First Commit

--- js/Drawmap.js
+++ js/Drawmap.js
@@ -18,26 +18,26 @@ var fDrawGroup= new L.FeatureGroup();
               );
               
             map.addControl(drawControl);
-            
+            var drawnItems = [];
               map.on("draw:created", function(e)
               {
                 var type = e.layerType;
-                var layer = e.layer;
+              var layer = e.layer;
 
-                // Convert the drawn layer to GeoJSON
-                var geoJsonLayer = layer.toGeoJSON();
+              // Convert the drawn layer to GeoJSON
+              var geoJsonLayer = layer.toGeoJSON();
 
-                // Add the GeoJSON object to your array
-                fDrawGroup.push(geoJsonLayer);
+              // Add the GeoJSON object to your array
+              drawnItems.push(geoJsonLayer);
 
-                // Optionally, you can add the layer to a feature group on the map
-                fDrawGroup.addLayer(layer);
+              // Add the layer to fDrawGroup (assuming fDrawGroup is a L.FeatureGroup)
+              fDrawGroup.addLayer(layer);
 
-                // You can do further processing with the GeoJSON object if needed
-                console.log("GeoJSON object:", geoJsonLayer);
+              // You can do further processing with the GeoJSON object if needed
+              console.log("GeoJSON object:", geoJsonLayer);
 
-                // Example: Alerting the GeoJSON string representation
-                alert(JSON.stringify(geoJsonLayer));
+              // Example: Alerting the GeoJSON string representation
+              alert(JSON.stringify(geoJsonLayer));
               });
         //edit colors
         var ctlStyle;
