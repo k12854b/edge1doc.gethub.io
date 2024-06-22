ca55722740f019150916f6aa9c825ba0d1ce41ef
k12854b <oc.yrn07@gmail.com>
2024-06-22 15:57:49 +0200

    First Commit

--- handleButtonsave.js
+++ handleButtonsave.js
@@ -23,14 +23,19 @@ function saveDrawnFeatures() {
 }
 
 function sendGeoJsonToServer(geoJsonData) {
-  fetch('/save-geojson', {
+  fetch('http://localhost:3000/save-geojson', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
     },
     body: JSON.stringify(geoJsonData)
   })
-  .then(response => response.json())
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
   .then(data => console.log('Success:', data))
   .catch((error) => console.error('Error:', error));
 }
