6b305da35993bd20a36246f425b56bf09a203f9c
k12854b <oc.yrn07@gmail.com>
2024-06-23 11:19:03 +0200

    First Commit

--- handleButtonsave.js
+++ handleButtonsave.js
@@ -1,4 +1,28 @@
-const socket = io('http://localhost:3000'); // Initialize socket connection
+function saveDrawnFeatures() {
+  const drawnItems = fDrawGroup.getLayers();
+  drawnItems.forEach(layer => {
+    const geoJson = layer.toGeoJSON();
+
+    // Retrieve the style settings from the layer itself
+    const styleSettings = layer.options;
+    
+    const properties = {
+      style: styleSettings
+    };
+
+    const geometry = geoJson.geometry;
+
+    const geoJsonData = {
+      "type": "Feature",
+      "properties": properties,
+      "geometry": geometry
+    };
+
+    sendGeoJsonToServer(geoJsonData);
+  });
+}
+//socket structure 
+const socket = io('http://localhost:3000');
 
 socket.on('connect', () => {
   console.log('Connected to server');
@@ -25,43 +49,4 @@ function sendGeoJsonToServer(geoJsonData) {
   })
   .then(data => console.log('Success:', data))
   .catch((error) => console.error('Error:', error));
-}
-
-function sendDrawnFeatures() {
-  const drawnItems = fDrawGroup.getLayers();
-  drawnItems.forEach(layer => {
-    const geoJson = layer.toGeoJSON();
-
-    const styleSettings = layer.options;
-
-    const properties = {
-      style: styleSettings
-    };
-
-    const geometry = geoJson.geometry;
-
-    const geoJsonData = {
-      "type": "Feature",
-      "properties": properties,
-      "geometry": geometry
-    };
-
-    sendGeoJsonToServer(geoJsonData);
-    socket.emit('send-geojson', geoJsonData); // Emit to the server via WebSocket
-  });
-}
-
-function addGeoJsonToMap(geoJsonData) {
-  const geoJsonLayer = L.geoJson(geoJsonData, {
-    onEachFeature: function (feature, layer) {
-      if (feature.properties && feature.properties.style && typeof layer.setStyle === 'function') {
-        layer.setStyle(feature.properties.style);
-      }
-      if (feature.properties && feature.properties.popupContent) {
-        layer.bindPopup(feature.properties.popupContent);
-      }
-    }
-  }).addTo(map);
-
-  map.fitBounds(geoJsonLayer.getBounds());
 }
