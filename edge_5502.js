e67381517ec4ce5084653fac9d4b9e19a82f2ae4
k12854b <oc.yrn07@gmail.com>
2024-06-28 20:47:04 +0200

    First Commit

--- edge_5502.js
+++ edge_5502.js
@@ -16,19 +16,19 @@ app.get('/', async (req, res) =>  {
 	  fs.createReadStream(__dirname+'/edge.html').pipe(res);
 	}
 );
-app.get('/p11', async (req, res) =>  {
+app.get('/InterfaceCivilProtection', async (req, res) =>  {
 	  console.log('EDGE 5502.OnIndexRequest');
-	  fs.createReadStream(__dirname+'/p11.html').pipe(res);
+	  fs.createReadStream(__dirname+'/InterfaceCivilProtection.html').pipe(res);
 	}
 );
-app.get('/p12', async (req, res) =>  {
+app.get('/InterfacePolice', async (req, res) =>  {
 	  console.log('EDGE 5502.OnIndexRequest');
-	  fs.createReadStream(__dirname+'/p12.html').pipe(res);
+	  fs.createReadStream(__dirname+'/InterfacePolice.html').pipe(res);
 	}
 );
-app.get('/p13', async (req, res) =>  {
+app.get('/InterfaceSAMU', async (req, res) =>  {
 	  console.log('EDGE 5502.OnIndexRequest');
-	  fs.createReadStream(__dirname+'/p13.html').pipe(res);
+	  fs.createReadStream(__dirname+'/InterfaceSAMU.html').pipe(res);
 	}
 );
 
